4cbca4cd81e1ca481e42f7d106a911e2ecb4c29a
nhhimlaa-16 <nadira.halim@northsouth.edu>
2025-04-19 20:56:50 +0600

    Add files via upload

---
+++ adoption_sec/adoption.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adoption Section</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="adoption_style.css">
+
+</head>
+<body style="background-image: url('../image/dog.jpg'); background-repeat: no-repeat; background-size: cover; background-attachment: fixed;">
+
+<?php include '../header/header.php'; ?>
+<div class="container">
+        <h1 class="title" >Adoption Section</h1>
+
+        <!-- Categories -->
+        <div class="categories">
+            <h1 style="color:lavender">Categories</h2>
+            <ul>
+                <h2>
+                <li><a href="../pet/dog.php">Dogs</a></li>
+                <li><a href="../pet/cat.php">Cats</a></li>
+                <li><a href="../pet/bird.php">Birds</a></li>
+                <li><a href="../pet/fish.php">Fishes</a></li>
+            
+                </h2>
+            </ul>
+        </div>
+
+        
+</body>
+</html>
