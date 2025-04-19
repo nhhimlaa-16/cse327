77f7a13318c8b423472886a74f7d156b82b441d1
nhhimlaa-16 <nadira.halim@northsouth.edu>
2025-04-19 20:59:53 +0600

    Add files via upload

---
+++ Homepage/homepage.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Paw me - Homepage</title>
+</head>
+<body>
+    <h1>Welcome to Paw me</h1>
+    <nav>
+        <ul>
+            <li><a href="shop.php">Shop</a></li>
+            <li><a href="vet_services.php">Vet Services</a></li>
+            <li><a href="account.php">Account</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
